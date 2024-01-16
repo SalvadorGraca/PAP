5fe661afb1935a61e3a082210e77e98274e4b57d
SalvadorGraca <151628229+SalvadorGraca@users.noreply.github.com>
2024-01-16 17:09:57 +0000

    Add files via upload

---
+++ PAP/login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" href="css.css">
+</head>
+
+<body>
+    <center>
+        <h1>login</h1>
+            <table>
+                <tr>
+                    <td>Username:</td>
+                    <td>
+                        <input type="text" name="Username">
+                    </td>
+                    <td>Password:</td>
+                    <td>
+                        <input type="password" name="Password">
+                    </td>
+                </tr>
+                <tr>
+                    <td><button type="submit" name="criar" value="Login">Login</button></td>
+                </tr>
+            </table>
+        </form>
+    </center>
+</body>
+
+</html>
